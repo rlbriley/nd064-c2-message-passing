d6ff1eb05801aa79facc3ff61755ef4de3bc8db7
Richard Briley <124118508+rlbriley@users.noreply.github.com>
2023-03-29 00:20:24 -0400

    Fix space issue

--- modules/api/app/udaconnect/controllers.py
+++ modules/api/app/udaconnect/controllers.py
@@ -19,6 +19,13 @@ api = Namespace("UdaConnect", description="Connections via geolocation.")  # noq
 
 # TODO: This needs better exception handling
 
+@api.errorhandler(400)
+def bad_request(e):
+    return jsonify(error=str(e)), 400
+
+@api.errorhandler(404)
+def resource_not_found(e):
+    return jsonify(error=str(e)), 404
 
 @api.route("/locations")
 @api.route("/locations/<int:location_id>")
@@ -89,12 +96,3 @@ class ConnectionDataResource(Resource):
             meters=distance,
         )
         return results
-
-
-@api.errorhandler(400)
-def bad_request(e):
-     return jsonify(error=str(e)), 400
-
-@api.errorhandler(404)
-def resource_not_found(e):
-    return jsonify(error=str(e)), 404
