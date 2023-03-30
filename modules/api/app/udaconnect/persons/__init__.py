f6bdf71e99a980e5a785f4a54cf28fdf1bf77ef9
Richard Briley <124118508+rlbriley@users.noreply.github.com>
2023-03-30 17:32:35 -0400

    Oops needed Person not Connection

--- modules/api/app/udaconnect/persons/__init__.py
+++ modules/api/app/udaconnect/persons/__init__.py
@@ -10,17 +10,17 @@ db = SQLAlchemy()
 
 
 def register_routes(api, root="api"):
-    from app.udaconnect.controllers import api as udaconnect_api
+    from controllers import api as udaconnect_api
 
-    api.add_namespace(udaconnect_api, path=f"/{root}")
+    add_namespace(udaconnect_api, path=f"/{root}")
 
 def create_app(env=None):
-    from app.config import config_by_name
-    from app.routes import register_routes
+    from config import config_by_name
+    from routes import register_routes
 
     app = Flask(__name__)
-    app.config.from_object(config_by_name[env or "test"])
-    api = Api(app, title="UdaConnect API", version="0.1.0")
+    config.from_object(config_by_name[env or "test"])
+    api = Api(app, title="UdaConnect API Persons", version="0.1.0")
 
     CORS(app)  # Set CORS for development
 
