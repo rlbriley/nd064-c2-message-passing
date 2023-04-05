63246c596a2ad19111a01d60788ff7e2684ec341
Richard Briley <124118508+rlbriley@users.noreply.github.com>
2023-04-04 22:03:31 -0400

    Remove thread stuff. It wasn't working.

--- modules/locations/app/__init__.py
+++ modules/locations/app/__init__.py
@@ -1,16 +1,9 @@
-import threading
-import logging
 from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_restx import Api
 from flask_sqlalchemy import SQLAlchemy
-from app.udaconnect.services import LocationService
-#from kafka.admin import KafkaAdminClient, NewTopic
 
 db = SQLAlchemy()
-
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger("udaconnect-locations")
 
 
 def create_app(env=None):
@@ -26,25 +19,8 @@ def create_app(env=None):
     register_routes(api, app)
     db.init_app(app)
 
-#    createKafkaTopics()
-
-    logger.info("Creating thread for location service create.")
-#    x = threading.Thread(target=LocationService.createThread, daemon=True)
-#    x.start()
-
     @app.route("/health")
     def health():
         return jsonify("Locations Microservice healthy")
 
     return app
-
-# def createKafkaTopics():
-#     logger.info("Creating kafka topic 'locations'.")
-#     admin_client = KafkaAdminClient(
-#         bootstrap_servers="localhost:9092",
-#         client_id='test'
-#     )
-
-#     topic_list = []
-#     topic_list.append(NewTopic(name="locations", num_partitions=1, replication_factor=1))
-#     admin_client.create_topics(new_topics=topic_list, validate_only=False)
