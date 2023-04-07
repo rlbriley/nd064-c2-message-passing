0ff6aea9a5d8147e58efb87a25c60e90c0d3f003
Richard Briley <124118508+rlbriley@users.noreply.github.com>
2023-04-07 01:03:07 -0400

    Another one.....

--- modules/locations-consumer/wsgi.py
+++ modules/locations-consumer/wsgi.py
@@ -103,17 +103,13 @@ class LocationService:
 
 
 def create_app(env=None):
-    from app.config import config_by_name
-    from app.routes import register_routes
 
     logger.info("create_app()")
     app = Flask(__name__)
-    app.config.from_object(config_by_name[env or "test"])
     api = Api(app, title="UdaConnect API Locations", version="0.1.0")
 
     CORS(app)  # Set CORS for development
 
-    register_routes(api, app)
     db.init_app(app)
 
 
