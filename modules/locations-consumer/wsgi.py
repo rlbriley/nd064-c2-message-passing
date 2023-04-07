ca4f20e3e15e6f0896159eaed96e4c34e5802959
Richard Briley <124118508+rlbriley@users.noreply.github.com>
2023-04-07 00:46:23 -0400

    Move everything into wsgi.py

--- modules/locations-consumer/wsgi.py
+++ modules/locations-consumer/wsgi.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 import logging
 import json
 
-from app import create_app
 from datetime import datetime
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
@@ -100,6 +99,28 @@ class LocationService:
         db.session.commit()
 
         return new_location
+
+
+def create_app(env=None):
+    from app.config import config_by_name
+    from app.routes import register_routes
+
+    logger.info("create_app()")
+    app = Flask(__name__)
+    app.config.from_object(config_by_name[env or "test"])
+    api = Api(app, title="UdaConnect API Locations", version="0.1.0")
+
+    CORS(app)  # Set CORS for development
+
+    register_routes(api, app)
+    db.init_app(app)
+
+
+    @app.route("/health")
+    def health():
+        return jsonify("Locations Microservice healthy")
+
+    return app
 
 app = create_app(os.getenv("FLASK_ENV") or "test")
 
