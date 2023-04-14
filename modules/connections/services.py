6b295e682d45a3d4c36768b8d8c249bbed581e36
Richard Briley <124118508+rlbriley@users.noreply.github.com>
2023-04-14 01:00:35 -0400

    Try11

--- modules/connections/services.py
+++ modules/connections/services.py
@@ -1,10 +1,9 @@
+from __future__ import annotations
 import os
 import logging
 from datetime import datetime, timedelta
 from typing import Dict, List
 
-from models import Connection, Location, Person
-from schemas import ConnectionSchema, LocationSchema, PersonSchema
 from geoalchemy2.functions import ST_AsText, ST_Point
 from sqlalchemy.sql import text, func
 from sqlalchemy import create_engine
@@ -12,6 +11,18 @@ from sqlalchemy.engine import URL
 from sqlalchemy import orm
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+from dataclasses import dataclass
+
+from geoalchemy2 import Geometry
+from geoalchemy2.shape import to_shape
+from shapely.geometry.point import Point
+from sqlalchemy import BigInteger, Column, Date, DateTime, ForeignKey, Integer, String
+from sqlalchemy.dialects.postgresql import JSONB, UUID
+from sqlalchemy.ext.hybrid import hybrid_property
+from geoalchemy2.types import Geometry as GeometryType
+from marshmallow import Schema, fields
+from marshmallow_sqlalchemy.convert import ModelConverter as BaseModelConverter
 
 logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger("grpc-connections")
@@ -122,3 +133,79 @@ class ConnectionService:
     @staticmethod
     def retrieve_allpersons() -> List[Person]:
         return session.query(Person).order_by(Person.id.asc()).all()
+
+class Person(Base):
+    __tablename__ = "person"
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    company_name = Column(String, nullable=False)
+
+
+class Location(Base):
+    __tablename__ = "location"
+
+    id = Column(BigInteger, primary_key=True)
+    person_id = Column(Integer, ForeignKey(Person.id), nullable=False)
+    coordinate = Column(Geometry("POINT"), nullable=False)
+    creation_time = Column(DateTime, nullable=False, default=datetime.utcnow)
+    _wkt_shape: str = None
+
+    @property
+    def wkt_shape(self) -> str:
+        # Persist binary form into readable text
+        if not self._wkt_shape:
+            point: Point = to_shape(self.coordinate)
+            # normalize WKT returned by to_wkt() from shapely and ST_AsText() from DB
+            self._wkt_shape = point.to_wkt().replace("POINT ", "ST_POINT")
+        return self._wkt_shape
+
+    @wkt_shape.setter
+    def wkt_shape(self, v: str) -> None:
+        self._wkt_shape = v
+
+    def set_wkt_with_coords(self, lat: str, long: str) -> str:
+        self._wkt_shape = f"ST_POINT({lat} {long})"
+        return self._wkt_shape
+
+    @hybrid_property
+    def longitude(self) -> str:
+        coord_text = self.wkt_shape
+        return coord_text[coord_text.find(" ") + 1 : coord_text.find(")")]
+
+    @hybrid_property
+    def latitude(self) -> str:
+        coord_text = self.wkt_shape
+        return coord_text[coord_text.find("(") + 1 : coord_text.find(" ")]
+
+
+@dataclass
+class Connection:
+    location: Location
+    person: Person
+
+class LocationSchema(Schema):
+    id = fields.Integer()
+    person_id = fields.Integer()
+    longitude = fields.String(attribute="longitude")
+    latitude = fields.String(attribute="latitude")
+    creation_time = fields.DateTime()
+
+    class Meta:
+        model = Location
+
+
+class PersonSchema(Schema):
+    id = fields.Integer()
+    first_name = fields.String()
+    last_name = fields.String()
+    company_name = fields.String()
+
+    class Meta:
+        model = Person
+
+
+class ConnectionSchema(Schema):
+    location = fields.Nested(LocationSchema)
+    person = fields.Nested(PersonSchema)
