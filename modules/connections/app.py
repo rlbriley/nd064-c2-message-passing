b026d02d8b36fa8bb295b3570ce104aad588e577
Richard Briley <124118508+rlbriley@users.noreply.github.com>
2023-04-16 22:24:34 -0400

    Ok here goes.

---
+++ modules/connections/app.py
@@ -0,0 +1,62 @@
+#!/sur/bin/env python3
+from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
+from flask import request
+from flask_accepts import accepts, responds
+from flask_restx import Namespace, Resource
+from typing import Optional, List
+from werkzeug.exceptions import abort
+
+import time
+from concurrent import futures
+
+import grpc
+import connections_pb2
+import connections_pb2_grpc
+import logging
+from marshmallow import Schema, fields
+from datetime import datetime, date
+from services import ConnectionService
+
+DATE_FORMAT = "%Y-%m-%d"
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger("connections")
+
+# Define the Flask application
+app = Flask(__name__)
+
+print("Sending sample payload...")
+
+channel = grpc.insecure_channel("localhost:5005")
+stub = connections_pb2_grpc.ConnectionsServiceStub(channel)
+
+# Initialize gRPC server
+server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
+connections_pb2_grpc.add_ConnectionsServiceServicer_to_server(ConnectionsServicer(), server)
+
+
+@app.route("/persons/<person_id>/connection")
+@app.param("start_date", "Lower bound of date range", _in="query")
+@app.param("end_date", "Upper bound of date range", _in="query")
+@app.param("distance", "Proximity to a given user in meters", _in="query")
+class ConnectionDataResource(Resource):
+#    @responds(schema=ConnectionSchema, many=True)
+    def get(self, person_id):
+        logger.debug(f"request: {request}")
+        start_date: datetime = datetime.strptime(
+            request.args["start_date"], DATE_FORMAT
+        )
+        end_date: datetime = datetime.strptime(request.args["end_date"], DATE_FORMAT)
+        distance: Optional[int] = request.args.get("distance", 5)
+
+        connQuery = connections_pb2.ConnectionQuery(
+            person_id,
+            start_date,
+            end_date,
+            distance )
+
+        results = stub.find_contacts(connQuery)
+
+        logger.deubg(f"Results: {results}")
+
+        return results
