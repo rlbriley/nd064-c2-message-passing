aef29aa675fab32e2808a1b7699e79c095f5cb7f
Richard Briley <124118508+rlbriley@users.noreply.github.com>
2023-04-21 01:27:30 -0400

    And again

--- modules/connections/app.py
+++ modules/connections/app.py
@@ -118,7 +118,7 @@ def get(person_id):
     logger.debug(f"Connections: {connections}")
 
     # convert from ConnectionList to JSON
-    if len(connections) > 0:
+    if connections.connections:
         conn_list = connlist_to_json( connections )
         response = app.response_class(
             response = conn_list,
